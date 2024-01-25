05597dbfd3906453db976116f9d511ac2a7425d4
JustinLTH3 <justinlth3@gmail.com>
2024-01-25 14:10:42 +0000

    Fixed Camera

--- Assets/Scripts/Spawner.cs
+++ Assets/Scripts/Spawner.cs
@@ -1,15 +1,18 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 
 public class Spawner : MonoBehaviour
 {
-    List<GameObject> spawnPos = new List<GameObject>();
+    List<GameObject> spawnPos = new();
+
     //Store loaded enemies, true == spawned
     Dictionary<GameObject, bool> enemies = new();
     List<Enemy> enemiesList = new();
     float spawnFreq = 5;
     public static Spawner Instance;
+    [SerializeField] private int TrackNum;
 
     private void Start()
     {
@@ -18,45 +21,53 @@ public class Spawner : MonoBehaviour
         enemyPrefab.AddRange(Resources.LoadAll<GameObject>("Enemies/"));
         for (int j = 0; j < 3; j++)
         {
-            for (int i = 0; i < enemyPrefab.Count; i++)
+            foreach (var key in enemyPrefab.Select(Instantiate))
             {
-                GameObject key = Instantiate(enemyPrefab[i]);
                 enemies.Add(key, false);
                 enemiesList.Add(key.GetComponent<Enemy>());
             }
         }
-        Camera.main.orthographicSize = 8 * Screen.height / Screen.width * 0.5f;
-        for (int i = 0; i < 3; i++)
+
+        Camera.main.orthographicSize = 8 * Screen.width / Screen.height * 0.5f;
+        for (int i = 0; i < TrackNum; i++)
         {
-            GameObject temp = new GameObject("spawnpos" + i);
-            temp.transform.position = new(2 * i - 2 * (3 - 3 % 2) / 2, 0, 0);
+            GameObject temp = new("spawnpos" + i)
+            {
+                transform =
+                {
+                    position = new Vector3(2 * i - (TrackNum - TrackNum % 2) / 2, 0, 0)
+                }
+            };
             spawnPos.Add(temp);
         }
+
         StartGame();
     }
-    void StartGame()
+
+    public void StartGame()
     {
         StartCoroutine(SpawnEnemy());
     }
+
     IEnumerator SpawnEnemy()
     {
-        WaitForSeconds waitForSeconds = new WaitForSeconds(spawnFreq);
+        WaitForSeconds waitForSeconds = new(spawnFreq);
         while (true)
         {
-
             int rand = Random.Range(0, enemies.Count);
             while (enemies[enemiesList[rand].gameObject])
             {
                 rand = Random.Range(0, enemies.Count);
             }
+
             enemies[enemiesList[rand].gameObject] = true;
             int rand2 = Random.Range(0, spawnPos.Count);
             enemiesList[rand].Spawn(spawnPos[rand2].transform.position);
 
             yield return waitForSeconds;
         }
-
     }
+
     public void Despawn(GameObject enemy)
     {
         enemies[enemy] = false;
