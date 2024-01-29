f083c3842d294df5ae0ef9f51dd4cd554610a435
papasgaming123 <periklaros31@gmail.com>
2024-01-29 16:18:48 +0000

    Kill Screen, can't replay.

--- Assets/Scripts/Spawner.cs
+++ Assets/Scripts/Spawner.cs
@@ -17,6 +17,7 @@ public class Spawner : MonoBehaviour
     [SerializeField] private int TrackNum;
     [SerializeField] private Transform[] lines = new Transform[2];
     Coroutine GameLoop;
+    [SerializeField]Animator KillScreen;
 
     private void Start()
     {
@@ -88,6 +89,10 @@ public class Spawner : MonoBehaviour
     }
     public void EndGame()
     {
+        
+        KillScreen.SetTrigger("Enable");
+
         StopCoroutine(GameLoop);
+
     }
 }
