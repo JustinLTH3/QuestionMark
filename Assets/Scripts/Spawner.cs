d4096be46f3b8285abc3f8c5979bafa9798e3623
papasgaming123 <periklaros31@gmail.com>
2024-01-30 11:44:05 +0000

    Pause Button

--- Assets/Scripts/Spawner.cs
+++ Assets/Scripts/Spawner.cs
@@ -4,6 +4,8 @@ using System.Linq;
 using TMPro;
 using UnityEngine;
 using UnityEngine.Serialization;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class Spawner : MonoBehaviour
 {
@@ -22,6 +24,9 @@ public class Spawner : MonoBehaviour
     float timer;
     [SerializeField] TMP_Text scoreDisplayInGame;
     [SerializeField] TMP_Text FinalScore;
+    public Button PauseButton;
+
+    bool isPaused = false;
 
     private void Start()
     {
@@ -93,6 +98,7 @@ public class Spawner : MonoBehaviour
     }
     public void EndGame()
     {
+        PauseButton.gameObject.SetActive(false);
         killscreen.SetTrigger("Enable");
         int score = (int)(Time.time - timer);
         FinalScore.text = score.ToString();
